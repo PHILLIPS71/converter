5eb322a95815866b360ea6decd959adfc16b619d
Jordan Phillips <jordypee27@gmail.com>
2024-10-29 09:39:17 +1100

    feat: add a way to rollback a uow + improve documentation (#1)

--- src/Infrastructure/src/Infrastructure/Uow/UnitOfWorkOptions.cs
+++ src/Infrastructure/src/Infrastructure/Uow/UnitOfWorkOptions.cs
@@ -4,7 +4,13 @@ namespace Giantnodes.Infrastructure;
 
 public sealed class UnitOfWorkOptions
 {
+    /// <summary>
+    /// Transaction scope option. Defaults to Required (uses existing transaction if available; otherwise, creates new).
+    /// </summary>
     public TransactionScopeOption Scope { get; init; } = TransactionScopeOption.Required;
 
+    /// <summary>
+    /// Transaction timeout. Null uses the system default timeout.
+    /// </summary>
     public TimeSpan? Timeout { get; init; }
 }

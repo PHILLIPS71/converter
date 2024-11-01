5f3b4f77600daa1c0912bd1b0a7238408fe6ccc1
Jordan Phillips <jordypee27@gmail.com>
2024-11-01 17:04:13 +1100

    feat: scan directory on library creation

---
+++ src/Service.Supervisor/src/Components/Libraries/Events/LibraryCreateScanDirectory.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.IO.Abstractions;
+using Giantnodes.Infrastructure;
+using Giantnodes.Service.Supervisor.Contracts.Libraries;
+using Giantnodes.Service.Supervisor.Domain.Aggregates.Entries.Directories;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace Giantnodes.Service.Supervisor.Components.Libraries.Events;
+
+public sealed partial class LibraryCreateScanDirectory : IConsumer<LibraryCreatedEvent>
+{
+    private readonly IFileSystem _fs;
+    private readonly IDirectoryRepository _directories;
+    private readonly ILogger<LibraryCreateScanDirectory> _logger;
+
+    public LibraryCreateScanDirectory(
+        IFileSystem fs,
+        IDirectoryRepository directories,
+        ILogger<LibraryCreateScanDirectory> logger)
+    {
+        _fs = fs;
+        _directories = directories;
+        _logger = logger;
+    }
+
+    [UnitOfWork]
+    public async Task Consume(ConsumeContext<LibraryCreatedEvent> context)
+    {
+        var directory = await _directories
+            .SingleAsync(x => x.Id == context.Message.DirectoryId, context.CancellationToken);
+
+        var stopwatch = Stopwatch.GetTimestamp();
+        var result = directory.TryScan(_fs);
+        if (result.IsError)
+        {
+            _logger.LogError(
+                "Directory scan failed after {ElapsedTime}. DirectoryId: {DirectoryId}, Path: {DirectoryPath}, Error: {ErrorMessage}",
+                Stopwatch.GetElapsedTime(stopwatch),
+                directory.Id,
+                directory.PathInfo.FullName,
+                result.FirstError.Description);
+            return;
+        }
+
+        _logger.LogInformation(
+            "Directory scan completed in {ElapsedTime}. DirectoryId: {DirectoryId}, Path: {DirectoryPath}", 
+            Stopwatch.GetElapsedTime(stopwatch),
+            directory.Id,
+            directory.PathInfo.FullName);
+    }
+}

ce6552b5b3078da3393064c249db7c26079d8ab2
Jordan Phillips <jordypee27@gmail.com>
2024-11-12 18:01:53 +1100

    fix: add utility function to convert a error to a fault kind

--- src/Infrastructure/src/Infrastructure/Faults/Extensions/ErrorExtensions.cs
+++ src/Infrastructure/src/Infrastructure/Faults/Extensions/ErrorExtensions.cs
@@ -4,6 +4,26 @@ namespace ErrorOr;
 
 public static class ErrorExtensions
 {
+    public static FaultKind ToFaultKind<TValue>(this ErrorOr<TValue> error)
+    {
+        return error.FirstError.ToFaultKind();
+    }
+
+    public static FaultKind ToFaultKind(this Error error)
+    {
+        return error.Type switch
+        {
+            ErrorType.Failure => FaultKind.Unexpected,
+            ErrorType.Unexpected => FaultKind.Unexpected,
+            ErrorType.Validation => FaultKind.Validation,
+            ErrorType.Conflict => FaultKind.Constraint,
+            ErrorType.NotFound => FaultKind.NotFound,
+            ErrorType.Unauthorized => FaultKind.Unexpected,
+            ErrorType.Forbidden => FaultKind.Unexpected,
+            _ => FaultKind.Unexpected,
+        };
+    }
+
     public static IEnumerable<DomainFault.ErrorInfo> ToFault<TValue>(this ErrorOr<TValue> error)
     {
         return error.Errors.Select(x => new DomainFault.ErrorInfo(x.Code, x.Description));

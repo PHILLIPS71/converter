f76b75315b6d3350ad4a69c58adf879c7fe852af
Jordan Phillips <jordypee27@gmail.com>
2024-11-19 16:54:15 +1100

    feat: add app settings + program setup

--- src/Service.Runner/src/Console/Program.cs
+++ src/Service.Runner/src/Console/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+internal sealed class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        try
+        {
+            var host = CreateHostBuilder(args).Build();
+            await host.RunAsync();
+            return 0;
+        }
+        catch (Exception)
+        {
+            return 1;
+        }
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((context, configuration) =>
+            {
+                configuration.AddJsonFile("appsettings.json", false);
+                configuration.AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true);
+                configuration.AddEnvironmentVariables();
+                configuration.AddCommandLine(args);
+            })
+            .UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+}

681b3c47c892689343d615bf622e857dac31772f
Jordan Phillips <jordypee27@gmail.com>
2025-01-09 18:52:34 +1100

    refactor: add pipeline abstractions csproj

--- src/Infrastructure/src/Pipelines.Abstractions/PipelineSpecificationDefinition.cs
+++ src/Infrastructure/src/Pipelines.Abstractions/PipelineSpecificationDefinition.cs
@@ -2,15 +2,6 @@
 
 namespace Giantnodes.Infrastructure.Pipelines;
 
-public sealed record PipelineDefinition
-{
-    public required string Name { get; init; }
-
-    public string? Description { get; init; }
-
-    public ICollection<PipelineSpecificationDefinition> Specifications { get; init; } = [];
-}
-
 public sealed record PipelineSpecificationDefinition
 {
     public required string Name { get; init; }

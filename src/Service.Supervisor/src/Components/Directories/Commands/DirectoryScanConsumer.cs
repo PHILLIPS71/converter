1e61468b5a609962855367586913c73cfd01344e
Jordan Phillips <jordypee27@gmail.com>
2025-01-21 13:24:09 +1100

    feat: add directory scan consumer and mutation

---
+++ src/Service.Supervisor/src/Components/Directories/Commands/DirectoryScanConsumer.cs
@@ -0,0 +1,30 @@
+﻿using ErrorOr;
+using Giantnodes.Infrastructure;
+using Giantnodes.Service.Supervisor.Contracts.Directories;
+using Giantnodes.Service.Supervisor.Domain.Aggregates.Entries.Directories;
+using MassTransit;
+
+namespace Giantnodes.Service.Supervisor.Components.Directories.Commands;
+
+public sealed partial class DirectoryScanConsumer : IConsumer<DirectoryScan.Command>
+{
+    private readonly IDirectoryScanningService _scanner;
+
+    public DirectoryScanConsumer(IDirectoryScanningService scanner)
+    {
+        _scanner = scanner;
+    }
+
+    [UnitOfWork]
+    public async Task Consume(ConsumeContext<DirectoryScan.Command> context)
+    {
+        var result = await _scanner.TryScanDirectoryAsync(context.Message.DirectoryId, context.CancellationToken);
+        if (result.IsError)
+        {
+            await context.RejectAsync(result.ToFaultKind(), result.ToFault());
+            return;
+        }
+
+        await context.RespondAsync(new DirectoryScan.Result { DirectoryId = context.Message.DirectoryId });
+    }
+}

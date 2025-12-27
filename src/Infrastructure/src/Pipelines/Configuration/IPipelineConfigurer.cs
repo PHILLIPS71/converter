312863ac5391a7561cb9d95d01b98e754729f04f
Jordan Phillips <jordypee27@gmail.com>
2025-12-28 10:45:13 +1100

    test: add PipelineConfigurerTests unit tests

--- src/Infrastructure/src/Pipelines/Configuration/IPipelineConfigurer.cs
+++ src/Infrastructure/src/Pipelines/Configuration/IPipelineConfigurer.cs
@@ -7,13 +7,13 @@ public interface IPipelineConfigurer
     /// <summary>
     /// Gets the service collection used to register pipeline dependencies.
     /// </summary>
-    IServiceCollection Services { get; }
+    public IServiceCollection Services { get; }
 
     /// <summary>
     /// Adds an operation type to the pipeline's available operations.
     /// </summary>
     /// <typeparam name="TOperation">The type of operation to add, which must implement <see cref="IPipelineOperation"/>.</typeparam>
     /// <returns>The same <see cref="IPipelineConfigurer"/> instance for method chaining.</returns>
-    IPipelineConfigurer AddOperation<TOperation>()
+    public IPipelineConfigurer AddOperation<TOperation>()
         where TOperation : IPipelineOperation;
 }

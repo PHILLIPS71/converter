bc70287071f8be1bd1bed8b25a69f4d976e7c3b0
Jordan Phillips <jordypee27@gmail.com>
2026-01-16 17:08:52 +1100

    chore: use slnx solution format (#33)
    * chore: migrate to sln to slnx format
    
    * fix: all.slnx solution containing project sub-directories

--- .build/Build.cs
+++ .build/Build.cs
@@ -2,33 +2,36 @@ using System.IO;
 using System.Linq;
 using Newtonsoft.Json;
 using Nuke.Common;
+using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
+using static Helpers;
 
 partial class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
     public static int Main() => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    private readonly Configuration _configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
     Target Clean => _ => _
         .Before(Restore)
         .Executes(() =>
         {
+            SourceDirectory
+                .GlobDirectories("**/bin", "**/obj")
+                .ForEach(x => x.DeleteDirectory());
         });
 
     Target Restore => _ => _
         .Executes(() =>
         {
-            Helpers.DotNetBuildSolution(AllSolutionFile);
-            DotNetRestore(x => x.SetProjectFile(AllSolutionFile));
+            EnsureSolutionExists(AllSolutionFile);
+
+            DotNetRestore(s => s
+                .SetProjectFile(AllSolutionFile));
         });
 
     Target Compile => _ => _
@@ -36,32 +39,54 @@ partial class Build : NukeBuild
         .Executes(() =>
         {
             if (!InvokedTargets.Contains(Restore))
-                Helpers.DotNetBuildSolution(AllSolutionFile);
+                EnsureSolutionExists(AllSolutionFile);
 
-            DotNetBuild(c => c
+            DotNetBuild(s => s
                 .SetProjectFile(AllSolutionFile)
-                .SetNoRestore(InvokedTargets.Contains(Restore))
-                .SetConfiguration(_configuration));
+                .SetConfiguration(Configuration)
+                .SetNoRestore(InvokedTargets.Contains(Restore)));
+        });
+
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            DotNetTest(s => s
+                .SetProjectFile(AllSolutionFile)
+                .SetConfiguration(Configuration)
+                .SetNoBuild(InvokedTargets.Contains(Compile))
+                .SetNoRestore(InvokedTargets.Contains(Restore)));
+        });
+
+    Target Reset => _ => _
+        .Executes(() =>
+        {
+            TryDelete(AllSolutionFile);
+            EnsureSolutionExists(AllSolutionFile);
+
+            DotNetRestore(s => s
+                .SetProjectFile(AllSolutionFile));
         });
 
     Target MatrixGenerate => _ => _
         .Executes(() =>
         {
-            Helpers.DotNetBuildSolution(AllSolutionFile);
-            var all = AllSolutionFile.ReadSolution();
+            EnsureSolutionExists(AllSolutionFile);
 
+            var solution = AllSolutionFile.ReadSolution();
             var matrix = new
             {
-                include = all
+                include = solution
                     .GetAllProjects("*.Tests")
                     .Select(p => new
                     {
                         name = Path.GetFileNameWithoutExtension(p.Path),
-                        path = Path.GetRelativePath(RootDirectory, p.Path),
+                        path = RootDirectory.GetRelativePathTo(p.Path).ToString(),
                         directoryPath = Path.GetDirectoryName(p.Path)
-                    }).ToArray()
+                    })
+                    .ToArray()
             };
 
-            File.WriteAllText(RootDirectory / "matrix.json", JsonConvert.SerializeObject(matrix));
+            File.WriteAllText(RootDirectory / "matrix.json", JsonConvert.SerializeObject(matrix, Formatting.Indented));
         });
 }
